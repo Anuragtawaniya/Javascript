f1a9da8407803b18034cf8362ebb1075cb037a39
anuragtawaniya <53399643+Anuragtawaniya@users.noreply.github.com>
2022-01-07 19:57:56 +0530

    Add files via upload

---
+++ colorFlipper/script.js
@@ -0,0 +1,14 @@
+// const colors = ["green","red","rgba(133,122,200)","#f15025"];
+
+// const btn = document.getElementById('btn');
+// const color = document.querySelector(".color");
+
+// btn.addEventListener('click',function(){
+// console.log(document.body);
+// })
+
+function sum(num1,num2){
+    var total = num1 + num2;
+    console.log(total);
+} 
+sum(6,5)
